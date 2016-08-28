cb148c79225096ec17c4b8c9291a7ead01292ddf
Keishi Mitsuhashi <mitsuhashi.keishi@gmail.com>
2016-08-28 15:43:44 +0900

    First Commit

---
+++ moyopi/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  moyopi
+//
+//  Created by Keishi Mitsuhashi on 2016/08/28.
+//  Copyright © 2016年 kebabees. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func btnCreate(sender: AnyObject) {
+        
+    }
+}
+
---
+++ moyopi/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  moyopi
+//
+//  Created by Keishi Mitsuhashi on 2016/08/28.
+//  Copyright © 2016年 kebabees. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
